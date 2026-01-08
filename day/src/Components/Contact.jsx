34b34aa1690e3d8f0ad4a497b7b7f0f2d9d83753
eeswari-s <eeswarivaithee@gmail.com>
2026-01-08 16:13:27 +0530

    c

--- day/src/Components/Contact.jsx
+++ day/src/Components/Contact.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import Footer from './Footer';
 
 function Contact() {
+    const a=10;
+    const message="hello";
   return (
     <div>
+
       <section className='px-20 py-16 text-center bg-gray-100'>
+              <h1>number:{a}</h1>
         <h1 className='text-3xl font-semibold mb-4'>Contact Us</h1>
         <p className='text-gray-600'>PHONE NUMBER:9087654321</p>
         <p className='text-gray-600'>B11SHOP@gmail.com</p>
         <p className='text-gray-600'>chennai</p>
       </section>
+      <Footer text={message} />
     </div>
   )
 }
--- day/src/Components/Footer.jsx
+++ day/src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Contact from './Contact';
 
 function Footer() {
   return (
@@ -6,6 +7,7 @@ function Footer() {
       <footer className='py-6 text-center bg-slate-900 text-white'>
         @2026 myshop. All rights reserved
       </footer>
+      <Contact />
     </div>
   );
 };
